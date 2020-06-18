f2bc6e5b44b3bcce87ce2ccae806351a5710337f
Ayush Raj <raj.ayush012@gmail.com>
2020-06-18 19:51:45 +0530

    UI up and working

--- UI/frontend/src/components/FileUploader.js
+++ UI/frontend/src/components/FileUploader.js
@@ -1,34 +1,81 @@
 import React, { Component, createRef } from 'react'
 import '../css/FileUploader.css'
+import IpOpList from './IpOpList'
 
-const { DefaultAzureCredential } = require("@azure/identity");
-const { BlobServiceClient } = require("@azure/storage-blob");
- 
 
-const account = "storageaccountgepte86f5";
-const defaultAzureCredential = new DefaultAzureCredential();
- 
-const blobServiceClient = new BlobServiceClient(
-  `https://${account}.blob.core.windows.net`,
-  defaultAzureCredential
-);
+const dotenv = require('dotenv')
 
+dotenv.config()
+
+const axios = require('axios')
 
 export class FileUploader extends Component {
     constructor(){
         super()
         this.state = {
             files : [],
+            converted : [],
+            result : true
             
         }
-        this.inputFileRef = createRef()
+        this.inputFileRef = createRef()        
+            }
+    
+    componentDidMount(){
+        this.getInputFiles()
+        this.getOutputFiles()
     }
-    
-    
 
-    uploadFiles = (files) => {
-        console.log(files)
+    uploadFiles =   (file) => {
+        const formData = new FormData();
+        console.log(file[0])
+        formData.append('asset',file[0])
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+        axios.post('http://techathon.azurewebsites.net/api/Files/InsertFile', formData, config)
+        .then(res=>{
+           // console.log('File Uploaded')
+           alert("File Uploaded Succesfully!")
+           this.getInputFiles()
+           
+        })
+        
     }
+
+    getInputFiles = () => {
+        axios.get('http://techathon.azurewebsites.net/api/Files/ListFiles')
+        .then(res => {
+           // console.log(res)
+            let filesCol = []
+            for(let f of res.data){
+                if( f.split('.').pop() !== 'csv')
+                    filesCol= [...filesCol,f]
+            }
+            this.setState({
+                files : filesCol
+            })
+        })
+    }
+
+    getOutputFiles = () => {
+        axios.get('http://techathon.azurewebsites.net/api/Files/ListFiles')
+        .then(res => {
+            let filesCol = []
+            for(let f of res.data){
+                if( f.split('.').pop() === 'csv')
+                    filesCol= [...filesCol,f]
+            }
+            this.setState({
+                converted : filesCol
+            })
+            //console.log(this.state.converted)
+        })
+    }
+   
+
 
     handleFileClick =() =>{
         this.inputFileRef.current && this.inputFileRef.current.click();
@@ -43,7 +90,7 @@ export class FileUploader extends Component {
                         name="input-file"
                         ref={this.inputFileRef}
                         type="file"
-                        multiple={true}
+                        multiple={false}
                         onChange={e => this.uploadFiles(e.target.files)}
                     />
                     <button className="btn"
@@ -52,7 +99,7 @@ export class FileUploader extends Component {
                         </button>
 
                         <hr></hr>
-                    
+                    <IpOpList getInputFiles={this.getInputFiles} getOutputFiles={this.getOutputFiles} inputFiles={this.state.files} outputFiles={this.state.converted}/>
                 </div>
         )
     }
